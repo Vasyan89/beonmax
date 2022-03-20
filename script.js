0fbb1652c3cb7b19d9b3ead017c938f43d895a4e
Vasyan89 <vasyan89@gmail.com>
2022-03-20 19:56:32 +0300

    ZZZ

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ const you = {
 }
 
 console.log(you['213123123123'])
+
+// Чтобы запустить этот скрипт напиши: node script.js
+
+function multByFactor (value, multiplier = 1) {
+    console.log(value * multiplier)
+
+    return value * multiplier
+}
+
+multByFactor(5, 10)
+
+const multiplier = (value, multiplier = 1) => {
+    console.log(value * multiplier)
+
+    return value * multiplier
+}
+
+multiplier(5)

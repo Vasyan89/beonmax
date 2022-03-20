20b9033154027a448f2a9aa36524d05ffa441198
Vasyan89 <vasyan89@gmail.com>
2022-03-20 19:46:49 +0300

    Z

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Пердежь")
+const you = {
+    age: 32,
+    name: 'Vasya',
+    favoriteBand: 'Erotic on Ren TV',
+    '213123123123': 'dsfsdsdfsdfsdf'
+}
+
+console.log(you['213123123123'])
